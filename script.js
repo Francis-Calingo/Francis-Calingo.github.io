afd02c363ee2e280775629efcb0b98754166c5da
Francis Emmanuel Calingo <83982370+Francis-Calingo@users.noreply.github.com>
2025-04-04 19:17:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ document.querySelectorAll("nav a").forEach(link => {
     this.classList.add("active");
   });
 });
+
+var map = L.map('map').setView([43.65107, -79.347015], 10); // Toronto coords
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
